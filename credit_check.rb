24254a9c8667b72f7864fd9876cd2c0ead1bdeb1
Mary Bork <maryebork@outlook.com>
2018-06-27 18:01:17 -0600

    Final-First Iteration

--- credit_check.rb
+++ credit_check.rb
@@ -1,16 +1,12 @@
 #INPUT:
 # Take A string of Numbers
-#Turn the Numbers into an array of Integers, Arranging them from last index to first
+#Turn the Numbers into an array of Integers(to access individual values), Arranging them from last index to first
 CreditNum = "6011797668867828" #(this is an integer)
 LuhnNum = CreditNum.to_i.digits #(empty array)
 puts "#{LuhnNum}"
 #______________________________
 #PROCESS:
-# Turn the integer of Numbers into an Array (to access individual values)
-# Flip the Array, arranging them from last index to first(Maybe step one--Reverse String)
-#(this flips the numbers unexpectedly? numbers in array dependent on base/radix?
-#Digits can also take arguments that change the return...? Dont really understand...but it works!)
-# Move through each 2nd element in the array and modify it by multiplying it by two (.map?)(.step?)(.each?)
+# Move through each 2nd element in the array and modify it by multiplying it by two (odd numbers to skip check number)
  DoublesArray = []
  LuhnNum.each.with_index do |num, index|
 	DoublesArray <<
@@ -21,6 +17,10 @@ puts "#{LuhnNum}"
 	end
 end
 puts "#{DoublesArray}"
+
+
+#Any Element with a value greater than 10 should be split and added together to get a single digit (or subtract by 9)
+
 SinglesArray = []
 DoublesArray.each.with_index do |num, index|
 	SinglesArray<<
@@ -30,38 +30,16 @@ DoublesArray.each.with_index do |num, index|
 			num
 		end
 	end
-	#AmendNum
-	#AmendNum << LuhnNum[i]
-
 	puts "#{SinglesArray}"
- #end
-# Any elements with a value greater than 10 should be split and added together to get a single digit number
-#DONT FORGET ABOUT THE CHECK DIGIT
-# You can add digits above 10 or subtract 9...≥Something like this...
+
 # Add all single resulting digits together to create a resulting number
-
 ResultNum = SinglesArray.sum
-
 print "#{ResultNum}"
-# Take the resulting number and modulo it with 10
+
+# Take the resulting number and modulo it with 10/ If == 0/VALID Else/INVALID
 ModNum = ResultNum % 10
 if ModNum == 0
 	puts "Valid Number"
 else
 	puts "Invalid Number"
 end
-
-#	def Luhn
-#	LuhnNum = CreditNum.reverse.to_i
-#
-#	(LuhnNum\change into array)
-#	LuhnNum.each do |arraynum|
-#	 LuhnArray = [arraynum.push]
-#	end
-#	end
-#-----------------------
-#OUTPUT:
-#If resulting number is == 0
-#Puts "Valid Number"
-#If resulting number is !=0
-#Puts "Invalid Number"
